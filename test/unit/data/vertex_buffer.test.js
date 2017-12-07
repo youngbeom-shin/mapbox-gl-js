a8d29975db7f1ccccccf43d3ba2a45baeafcbd4d
Anand Thakker <github@anandthakker.net>
2017-12-06 22:24:57 -0500

    Fixup unit tests

--- test/unit/data/vertex_buffer.test.js
+++ test/unit/data/vertex_buffer.test.js
@@ -2,19 +2,15 @@
 
 const test = require('mapbox-gl-js-test').test;
 const VertexBuffer = require('../../../src/gl/vertex_buffer');
-const StructArrayType = require('../../../src/util/struct_array');
+const StructArrayLayout3i = require('../../../src/data/array_type/struct_array_layout_1_3i');
 const Context = require('../../../src/gl/context');
 
 test('VertexBuffer', (t) => {
-
-    const TestArray = new StructArrayType({
-        members: [
-            { type: 'Int16', name: 'map' },
-            { type: 'Int16', name: 'box', components: 2 }
-        ],
-        alignment: 4
-    });
-
+    class TestArray extends StructArrayLayout3i {}
+    TestArray.prototype.members = [
+        { name: 'map', components: 1, type: 'Int16', offset: 0 },
+        { name: 'box', components: 2, type: 'Int16', offset: 4 }
+    ];
 
     t.test('constructs itself', (t) => {
         const context = new Context(require('gl')(10, 10));
@@ -26,19 +22,10 @@ test('VertexBuffer', (t) => {
         const buffer = new VertexBuffer(context, array);
 
         t.deepEqual(buffer.attributes, [
-            {
-                name: 'map',
-                components: 1,
-                type: 'Int16',
-                offset: 0
-            }, {
-                name: 'box',
-                components: 2,
-                type: 'Int16',
-                offset: 4
-            }]);
-
-        t.deepEqual(buffer.itemSize, 8);
+            { name: 'map', components: 1, type: 'Int16', offset: 0 },
+            { name: 'box', components: 2, type: 'Int16', offset: 4 }
+        ]);
+        t.deepEqual(buffer.itemSize, 6);
         t.deepEqual(buffer.length, 3);
         t.end();
     });
