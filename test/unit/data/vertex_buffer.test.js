0ecd677f7309fc34380443092b914fa4eb86e160
Anand Thakker <github@anandthakker.net>
2017-12-11 21:43:45 -0500

    Fix (and add to) vertex_buffer.test.js

--- test/unit/data/vertex_buffer.test.js
+++ test/unit/data/vertex_buffer.test.js
@@ -2,12 +2,12 @@
 
 const test = require('mapbox-gl-js-test').test;
 const VertexBuffer = require('../../../src/gl/vertex_buffer');
-const StructArrayLayout3i = require('../../../src/data/array_type/struct_array_layout_1_3i');
+const StructArrayLayout3i = require('../../../src/data/array_type/struct_array_layout_6_3i');
 const Context = require('../../../src/gl/context');
 
 test('VertexBuffer', (t) => {
     class TestArray extends StructArrayLayout3i {}
-    TestArray.prototype.members = [
+    const attributes = [
         { name: 'map', components: 1, type: 'Int16', offset: 0 },
         { name: 'box', components: 2, type: 'Int16', offset: 4 }
     ];
@@ -19,7 +19,7 @@ test('VertexBuffer', (t) => {
         array.emplaceBack(1, 1, 1);
         array.emplaceBack(1, 1, 1);
 
-        const buffer = new VertexBuffer(context, array);
+        const buffer = new VertexBuffer(context, array, attributes);
 
         t.deepEqual(buffer.attributes, [
             { name: 'map', components: 1, type: 'Int16', offset: 0 },
@@ -29,6 +29,30 @@ test('VertexBuffer', (t) => {
         t.deepEqual(buffer.length, 3);
         t.end();
     });
+
+    t.test('enableAttributes', (t) => {
+        const context = new Context(require('gl')(10, 10));
+        const array = new TestArray();
+        const buffer = new VertexBuffer(context, array, attributes);
+        t.stub(context.gl, 'enableVertexAttribArray').callsFake(() => {});
+        buffer.enableAttributes(context.gl, { attributes: { map: 5, box: 6 } });
+        t.deepEqual(context.gl.enableVertexAttribArray.args, [[5], [6]]);
+        t.end();
+    });
+
+    t.test('setVertexAttribPointers', (t) => {
+        const context = new Context(require('gl')(10, 10));
+        const array = new TestArray();
+        const buffer = new VertexBuffer(context, array, attributes);
+        t.stub(context.gl, 'vertexAttribPointer').callsFake(() => {});
+        buffer.setVertexAttribPointers(context.gl, { attributes: { map: 5, box: 6 } }, 50);
+        t.deepEqual(context.gl.vertexAttribPointer.args, [
+            [5, 1, context.gl['SHORT'], false, 6, 300],
+            [6, 2, context.gl['SHORT'], false, 6, 304]
+        ]);
+        t.end();
+    });
+
 
     t.end();
 });
